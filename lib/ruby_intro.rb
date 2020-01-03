8d52715adcae043a8c7cac12cc4474b4fe457316
Ubuntu <ubuntu@ip-172-31-34-23.us-east-2.compute.internal>
2020-01-03 04:59:14 +0000

    Part 1 and 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,28 +4,44 @@
 
 def sum arr
   # YOUR CODE HERE
+  result = 0
+  arr.each {|x| result += x}
+  result
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  max = arr.max(2)
+  max.sum
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
-end
+  arr.combination(2) { |value| return true if value.sum == n} 
+  false
+end 
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  a = "Hello, "
+  a << name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  Regexp.new('^[qwrtypsdfghjklzxcvbnm]', Regexp:: IGNORECASE).match?(s)
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  # if /^[01]+$/.match?(s)
+	#   s.to_i(2) % 4 == 0
+  # else
+  #	  false
+  # end
+  /^(0|00|([01]*00))$/.match?(s)
 end
 
 # Part 3
